1754326f02d3143debb4ca3418ea22a5e43b383e
Poteko Sakamoto <hmasterwang@outlook.com>
2020-09-02 04:09:50 +0000

    Merged PR 31: add signals to zWaifuEngine
    add signals to zWaifuEngine

--- include/IzWaifuEngine.h
+++ include/IzWaifuEngine.h
@@ -1,6 +1,9 @@
 #ifndef IZWAIFUENGINE_H
 #define IZWAIFUENGINE_H
 
+#include <spdlog/spdlog.h>
+
+#include <QObject>
 #include <QList>
 #include <QString>
 
@@ -16,18 +19,27 @@ enum class zWaifuMotionPriority {
 
 class IzWaifuEngine
 {
+protected:
+    IzWaifuEngine() = default;
+
 public:
     virtual ~IzWaifuEngine() = default;
 
+    virtual spdlog::logger* logger() = 0;
+
     virtual QQmlEngine *qmlEngine() = 0;
     virtual QQuickItem *rootElement() = 0;
-
     virtual QList<QString> expressions() = 0;
     virtual bool setExpression(const QString &expression) = 0;
 
     virtual QList<QString> motionGroups() = 0;
     virtual quint32 motionCount(const QString &motionGroup) = 0;
     virtual bool setMotion(const QString &motionGroup, quint32 id, zWaifuMotionPriority motionPriority) = 0;
+
+signals:
+    virtual void characterChanged(const QString &characterName) = 0;
+    virtual void expressionChanged(const QString &expression) = 0;
+    virtual void motionChanged(const QString &motionGroup, int id) = 0;
 };
 
 #endif // IZWAIFUENGINE_H
